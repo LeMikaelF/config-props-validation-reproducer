8b0cac48db238332458c5c50006401a1c637cc5b
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2024-04-13 13:17:17 -0400

    Add nested reproducer.

--- src/test/java/com/mikaelfrancoeur/validationreproducer/ValidationReproducerTest.java
+++ src/test/java/com/mikaelfrancoeur/validationreproducer/ValidationReproducerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
@@ -29,11 +30,27 @@ class ValidationReproducerTest implements WithAssertions {
                 .hasMessageContaining("Field error in object 'props' on field 'myprop': rejected value [null]"));
     }
 
+    @Test
+    void nested() {
+        runner
+                .withPropertyValues("props.myprop=value", "props.nested.myprop1=value")
+                .run(context -> assertThat(context).getFailure().rootCause()
+                .hasMessageContaining("Field error in object 'props.nested' on field 'myprop2': rejected value [null]"));
+    }
+
     @Data
     @Validated
     @ConfigurationProperties(prefix = "props")
     static class ConfigPropsClass {
         @NotEmpty private String myprop;
+
+        private Nested nested = new Nested();
+
+        @Data
+        static class Nested {
+            String myprop1;
+            @NotEmpty String myprop2;
+        }
     }
 
     @EnableConfigurationProperties(ConfigPropsClass.class)
