6e1fc4517a9e18f9c44e7b60485b963ac7dd3238
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2024-04-13 10:49:01 -0400

    Initial commit.

---
+++ src/test/java/com/mikaelfrancoeur/validationreproducer/ValidationReproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikaelfrancoeur.validationreproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationReproducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
