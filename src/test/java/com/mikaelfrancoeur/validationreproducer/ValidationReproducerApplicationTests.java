fe6add3cbe5bfcf70ba416b123d1ef218225016d
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2024-04-13 11:22:45 -0400

    Reproducer for record in a map.

--- src/test/java/com/mikaelfrancoeur/validationreproducer/ValidationReproducerApplicationTests.java
+++ src/test/java/com/mikaelfrancoeur/validationreproducer/ValidationReproducerApplicationTests.java
@@ -1,13 +1,58 @@
 package com.mikaelfrancoeur.validationreproducer;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.validation.annotation.Validated;
 
-@SpringBootTest
-class ValidationReproducerApplicationTests {
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
 
-    @Test
-    void contextLoads() {
+class ValidationReproducerTest implements WithAssertions {
+
+    private ApplicationContextRunner runner;
+
+    @BeforeEach
+    void beforeEach() {
+        runner = new ApplicationContextRunner()
+                .withConfiguration(AutoConfigurations.of(ValidationAutoConfiguration.class))
+                .withBean(TestConfig.class);
     }
 
+    @Test
+    void topLevelString() {
+        runner
+                .withPropertyValues("props.nesteds.something.prop1=something")
+                .run(context -> {
+                    assertThat(context)
+                            .getFailure()
+                            .hasMessage("");
+                });
+    }
+
+    @Data
+    @Validated
+    @ConfigurationProperties(prefix = "props")
+    static class ConfigPropsClass {
+
+        private Map<String, Nested> nesteds = new HashMap<>();
+
+        public record Nested(
+                String prop1,
+                @NotEmpty String prop2
+        ) {
+        }
+    }
+
+    @EnableConfigurationProperties(ConfigPropsClass.class)
+    static class TestConfig {
+    }
 }
