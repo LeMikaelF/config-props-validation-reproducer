04f7ef3bafc3615ed8fcce95c00c7769b01b28ad
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2024-04-13 13:11:46 -0400

    Keep only top-level reproducer.

--- src/test/java/com/mikaelfrancoeur/validationreproducer/ValidationReproducerTest.java
+++ src/test/java/com/mikaelfrancoeur/validationreproducer/ValidationReproducerTest.java
@@ -1,10 +1,5 @@
 package com.mikaelfrancoeur.validationreproducer;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,40 +24,18 @@ class ValidationReproducerTest implements WithAssertions {
     }
 
     @Test
-    void nestedClassMap() {
-        runner.withPropertyValues("props.nested-map.something.prop1=myvalue")
-                .run(context -> assertThat(context).getFailure().rootCause()
-                        .hasMessageContaining("Field error in object 'props.nested-map.something' on field 'prop2': rejected value [null]"));
-    }
-
-    @Test
-    void nestedClassSet() {
-        runner.withPropertyValues("props.nested-set[0].prop1=myvalue")
-                .run(context -> assertThat(context).getFailure().rootCause()
-                        .hasMessageContaining("Field error in object 'props.nested-set[0]' on field 'prop2': rejected value [null]"));
-    }
-
-    @Test
     void topLevel() {
-        runner.withPropertyValues("props.top-level.prop1=myvalue")
+        runner.withPropertyValues("props.prop1=myvalue")
                 .run(context -> assertThat(context).getFailure().rootCause()
-                        .hasMessageContaining("Field error in object 'props.top-level' on field 'prop2': rejected value [null]"));
+                        .hasMessageContaining("Field error in object 'props' on field 'prop2': rejected value [null]"));
     }
 
     @Data
     @Validated
     @ConfigurationProperties(prefix = "props")
     static class ConfigPropsClass {
-
-        private Map<String, MyClass> nestedMap = new HashMap<>();
-        private Set<MyClass> nestedSet = new HashSet<>();
-        private MyClass topLevel = new MyClass();
-
-        @Data
-        static class MyClass {
-            String prop1;
-            @NotEmpty String prop2;
-        }
+        private String prop1;
+        @NotEmpty private String prop2;
     }
 
     @EnableConfigurationProperties(ConfigPropsClass.class)
